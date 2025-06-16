16d1f462da683c8d404973a4ef815c8f3e5cd4f4
wallago <45556867+wallago@users.noreply.github.com>
2025-06-16 18:03:36 +0200

    refactor(components/lists): update it

--- src/components/list/mod.rs
+++ src/components/list/mod.rs
@@ -1,2 +1,4 @@
-pub mod model;
+mod model;
+pub use model::List;
+
 pub mod row;
--- src/components/list/row.rs
+++ src/components/list/row.rs
@@ -1,12 +1,87 @@
-use super::model::List;
+use crate::components::{
+    icons::{More, arrow},
+    list::List,
+    task::{
+        Task,
+        row::{TaskRow, TaskRowTitle},
+    },
+};
 use dioxus::prelude::*;
+
+static API: &str = "http://0.0.0.0:8081/api";
 
 #[component]
 pub fn ListRow(list: List) -> Element {
+    let mut is_open = use_signal(|| false);
+    let list_clone = list.clone();
+    let tasks = use_resource(move || {
+        let uuid = list_clone.uuid.clone();
+        async move {
+            let response = reqwest::get(format!("{API}/lists/{}/tasks", uuid))
+                .await
+                .ok()?;
+            response.json::<Vec<Task>>().await.ok()
+        }
+    });
+
     rsx!(li {
-        class: "pt-1 border-t-2 border-border-light dark:border-border-dark",
         key: "{list.uuid}",
-        "{list.name}"
+        class: "text-sm border-t-2 border-border-light dark:border-border-dark pt-2",
+        div {
+            class: "grid grid-cols-4 gap-4",
+            p {"{list.name}"}
+            p { "{list.description.as_deref().unwrap_or(\"-\")}" }
+            match tasks.read().as_ref() {
+                Some(Some(list_task_items)) => rsx!(
+                    p {
+                        class: "flex justify-between",
+                        "{list_task_items.len()}"
+                        if !list_task_items.is_empty() {
+                            p {
+                                class: "flex font-bold",
+                                "lists"
+                                button {
+                                    class: "ml-1 transition-transform duration-200",
+                                    class: if *is_open.read() { "rotate-180" } else { "rotate-0" },
+                                    onclick: move |_| is_open.toggle(),
+                                    arrow::Bottom {}
+                                }
+                            }
+                        }
+                    }
+                    button {
+                        class: "justify-self-end mr-4 h-6 w-8 border-border-light dark:border-border-dark hover:border-2 hover:bg-muted-light dark:hover:bg-muted-dark",
+                        div {
+                            class: "flex justify-center items-center w-full h-full",
+                            More {}
+                        }
+                    }
+                    if *is_open.read() {
+                        ul {
+                            class: "py-2 px-4 mb-2 border-2 border-border-light dark:border-border-dark bg-muted-light dark:bg-muted-dark w-full col-span-4 grid grid-cols-1 gap-2",
+                            TaskRowTitle {}
+                            Fragment {
+                                for task in list_task_items {
+                                    TaskRow { task: task.clone()  }
+                                }
+                            }
+                        }
+                    }
+                ),
+                Some(None) => rsx!(p {
+                    class: "text-error-light dark:text-error-dark",
+                    "failed to fetch"
+                }),
+                None => rsx!(p {
+                    ul {
+                        class: "space-y-2 animate-pulse",
+                        for i in 0..3 {
+                            ListRowSkeleton { i }
+                        }
+                    }
+                })
+            }
+        }
     })
 }
 
