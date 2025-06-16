ddc3269bceea8dba0c6061cc1d20c335dc5ce3ce
wallago <45556867+wallago@users.noreply.github.com>
2025-06-16 16:49:43 +0200

    feat(tailwindcss): add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,37 @@ module.exports = {
     },
   },
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        frida: ['"Frida Code"', "monospace"],
+      },
+      colors: {
+        surface: {
+          light: "#d8f3dc",
+          dark: "#240046",
+        },
+        text: {
+          light: "#081c15",
+          dark: "#e0aaff",
+        },
+        accent: {
+          light: "#52b788",
+          dark: "#7b2cbf",
+        },
+        border: {
+          light: "#95d5b2",
+          dark: "#5a189a",
+        },
+        muted: {
+          light: "#b7e4c7",
+          dark: "#3c096c",
+        },
+        error: {
+          light: "#6e1423",
+          dark: "#6e1423",
+        },
+      },
+    },
   },
   plugins: [],
 };
