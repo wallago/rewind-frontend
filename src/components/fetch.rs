8b29798283861ae86d66fe7781dae87282dce654
wallago <45556867+wallago@users.noreply.github.com>
2025-06-23 15:29:52 +0200

    feat(components/fetch): add board + get boards with retry

--- src/components/fetch.rs
+++ src/components/fetch.rs
@@ -1,20 +1,46 @@
 use dioxus::prelude::*;
-use serde::Deserialize;
+use futures_timer::Delay;
+use serde::Serialize;
 
-use crate::components::{
-    board::{
-        Board,
-        row::{BoardRow, BoardRowSkeleton, BoardRowTitle},
-    },
-    icons::arrow,
-};
+use crate::components::board::Board;
 
 static API: &str = "http://0.0.0.0:8081/api";
 
-pub fn get_boards() -> Result<Resource<Option<Vec<Board>>>, RenderError> {
-    let boards = use_resource(move || async move {
-        let response = reqwest::get(format!("{API}/boards",)).await.ok()?;
-        response.json::<Vec<Board>>().await.ok()
-    });
-    Ok(boards)
+pub fn get_boards(refetch_signal: Signal<u32>) -> Resource<Option<Vec<Board>>> {
+    use_resource(move || {
+        let _ = refetch_signal();
+        async move {
+            let mut retries = 0;
+            loop {
+                match reqwest::get(format!("{API}/boards")).await {
+                    Ok(res) => match res.json::<Vec<Board>>().await {
+                        Ok(boards) => break Some(boards),
+                        Err(_) if retries < 3 => retries += 1,
+                        Err(_) => break None,
+                    },
+                    Err(_) if retries < 3 => retries += 1,
+                    Err(_) => break None,
+                }
+
+                Delay::new(std::time::Duration::from_secs(2)).await;
+            }
+        }
+    })
+}
+
+#[derive(Serialize, Clone)]
+pub struct NewBoard {
+    pub name: String,
+    pub description: Option<String>,
+}
+
+pub async fn add_board(board: NewBoard) -> Option<Board> {
+    let client = reqwest::Client::new();
+    let response = client
+        .post(format!("{API}/boards"))
+        .json(&board)
+        .send()
+        .await
+        .ok()?;
+    response.json::<Board>().await.ok()
 }
