974ce64743c7c32c51d94d2faf0e7d3b70f9f3cd
wallago <45556867+wallago@users.noreply.github.com>
2025-06-23 15:29:14 +0200

    feat(component): add button

--- src/components/button.rs
+++ src/components/button.rs
@@ -1,20 +1,46 @@
 use dioxus::prelude::*;
 
-use super::modal;
-
 #[derive(PartialEq, Clone, Props)]
 pub struct ClickableProps {
     children: Element,
+    #[props(optional)]
+    class: Option<String>,
+    #[props(optional)]
+    onclick: Option<EventHandler<MouseEvent>>,
 }
 
 #[component]
 pub fn Button(props: ClickableProps) -> Element {
-    let mut modal_state = use_context::<modal::ModalState>();
+    let mut active = use_signal(|| false);
+    let default_classes = "
+        button-darken-effect
+        p-1
+        font-medium text-xs
+        bg-surface-light dark:bg-surface-dark
+        border-2 border-border-light dark:border-border-dark
+        hover:bg-surface-variant-light dark:hover:bg-surface-variant-dark
+        active:bg-border-light dark:active:bg-border-dark
+        transition duration-400
+    ";
 
-    // let mut dark_mode = use_context::<Signal<DarkMode>>();
-    rsx!(button {
-        onclick: move |_| modal_state.is_open.toggle(),
-        class: "p-1 border-2 border-border-light dark:border-border-dark  hover:bg-border-light dark:hover:bg-border-dark",
-        {props.children}
-    })
+    let onclick = {
+        move |evt| {
+            active.set(true);
+            if let Some(on_user_click) = &props.onclick {
+                on_user_click.call(evt);
+            }
+        }
+    };
+    rsx!(
+        button {
+            onclick: onclick,
+            class: format!(
+                "{} {} {}",
+                props.class.clone().unwrap_or_default(),
+                default_classes,
+                if *active.read() { " active" } else { "" }
+            ),
+            {props.children}
+        }
+    )
 }
