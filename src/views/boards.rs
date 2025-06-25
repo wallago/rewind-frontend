9bea136752c97597317d1dbdddc72445c4da6e77
wallago <45556867+wallago@users.noreply.github.com>
2025-06-25 12:18:15 +0200

    feat(views/boards): add drag behavior

--- src/views/boards.rs
+++ src/views/boards.rs
@@ -19,11 +19,12 @@ pub struct Board {
     pub uuid: String,
     name: String,
     description: Option<String>,
+    position: i32,
 }
 
 #[component]
 pub fn Boards() -> Element {
-    let refetch_signal = use_signal(|| 0);
+    let mut refetch_signal = use_signal(|| 0);
     let boards = fetch::boards::get_boards(refetch_signal);
 
     rsx! (
@@ -44,7 +45,7 @@ pub fn Boards() -> Element {
                         TableHead { "Description" }
                         TableHead {
                             class: Some("text-right".to_string()),
-                            DialogAdd { refetch_signal }
+                            DialogAdd { refetch_signal, boards }
                         }
                     }
                 }
@@ -59,17 +60,37 @@ pub fn Boards() -> Element {
                                     } })
                                 } else {
                                     let boards = boards.clone();
+                                    let mut dragging_board_uuid = use_signal(|| None::<String>);
                                     rsx!(
                                         {boards.iter().map(|board| {
                                             let uuid = board.uuid.clone();
                                             let name = board.name.clone();
                                             let desc = board.description.clone().unwrap_or_default();
+
+                                            let dragged_uuid_from = board.uuid.clone();
+                                            let dragged_uuid_to = board.uuid.clone();
                                             rsx!(TableRow {
                                                 class: "cursor-pointer hover:bg-muted-light dark:hover:bg-muted-dark",
                                                 onclick: move |e: MouseEvent| {
                                                     e.stop_propagation();
                                                     navigator().push(Route::Lists { uuid: uuid.clone() });
                                                 },
+                                                draggable: Some(true),
+                                                ondragstart: Some(EventHandler::new(move |_: DragEvent| {
+                                                    dragging_board_uuid.set(Some(dragged_uuid_from.clone()));
+                                                })),
+                                                ondrop: Some(EventHandler::new(move |_: DragEvent| {
+                                                    let uuid_to = dragged_uuid_to.clone();
+                                                    if let Some(uuid_from) = dragging_board_uuid() {
+                                                        spawn_local(async move {
+                                                            match fetch::boards::switch_boards(&uuid_from, &uuid_to).await {
+                                                                Ok(_) => refetch_signal.set(refetch_signal() + 1),
+                                                                Err(err) => tracing::error!("Update board failed with: {err}"),
+                                                            }
+                                                        })
+                                                    }
+                                                })),
+
 
                                                 TableCell {
                                                     class: Some("font-medium".to_string()),
@@ -99,9 +120,36 @@ pub fn Boards() -> Element {
 }
 
 #[component]
-pub fn DialogAdd(refetch_signal: Signal<u32>) -> Element {
+pub fn DialogAdd(refetch_signal: Signal<u32>, boards: Resource<Option<Vec<Board>>>) -> Element {
     let name = use_signal(|| "X".to_string());
     let description = use_signal(|| "".to_string());
+
+    let position = match boards() {
+        Some(Some(boards)) => {
+            let mut used_positions: Vec<i32> = boards.iter().map(|board| board.position).collect();
+            used_positions.sort_unstable();
+            used_positions.dedup();
+
+            let mut next_position = 0;
+
+            for &pos in &used_positions {
+                if pos == next_position {
+                    next_position += 1;
+                } else if pos > next_position {
+                    break;
+                }
+            }
+
+            next_position
+        }
+        Some(None) => 0,
+        None => {
+            return rsx!(Button {
+                disabled: true,
+                Add { size: "14px"}
+            });
+        }
+    };
 
     rsx!(DialogForm {
         title: String::from("Add Board"),
@@ -111,6 +159,7 @@ pub fn DialogAdd(refetch_signal: Signal<u32>) -> Element {
                 let board = fetch::boards::NewBoard {
                     name: name(),
                     description: Some(description()),
+                    position: position,
                 };
 
                 match fetch::boards::add_board(board).await {
@@ -158,6 +207,7 @@ pub fn DialogUpdate(refetch_signal: Signal<u32>, board: Board) -> Element {
                 let board = fetch::boards::UpdateBoard {
                     name: Some(name()),
                     description: Some(description()),
+                    position: None,
                 };
 
                 match fetch::boards::update_board(&uuid, board).await {
